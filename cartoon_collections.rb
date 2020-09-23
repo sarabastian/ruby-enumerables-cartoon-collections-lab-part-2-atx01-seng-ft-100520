3357a7dbacae63afeaf866e42c2d3caa32e7412d
Sara Bastian <seb118@georgetown.edu>
2020-09-23 17:04:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do ||
+  planeteer_calls.find do |element|
     
   end 
   
